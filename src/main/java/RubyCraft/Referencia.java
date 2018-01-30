d9a663c7c468704e6f956ef979c0fe00223202b2
JoelGamer2 <joelgamer2@outlook.com>
2018-01-30 19:41:11 +0100

    poner 1.0

--- src/main/java/RubyCraft/Referencia.java
+++ src/main/java/RubyCraft/Referencia.java
@@ -4,7 +4,7 @@ public class Referencia {
  
 	public static final String MOD_ID = "rc";
 	public static final String NAME = "RubyCraft";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	
 	public static final String CLIENT_PROXY_CLASS = "RubyCraft.proxy.ClientProxy";
