c88ca47973cda37417a1024122f6fb4e6a58a6f0
JoelGamer2 <joelgamer2@outlook.com>
2018-06-02 13:19:40 +0200

    cambios del tiempo 7u7

--- src/main/java/RubyCraft/ArmadurasConEventosRaros/casco_de_tortuga.java
+++ src/main/java/RubyCraft/ArmadurasConEventosRaros/casco_de_tortuga.java
@@ -20,6 +20,10 @@ import net.minecraft.world.World;
 public class casco_de_tortuga extends ItemArmor {
 	
 	public static boolean Colocar = false;
+	public static boolean lloviendo = false;
+	public static boolean soleado = false;
+	public static boolean truenos = false;
+	
 	
 	public casco_de_tortuga(ArmorMaterial material, int render, EntityEquipmentSlot donde_se_equipa, String unlocalizedName) {
 		super(material, render, donde_se_equipa);
@@ -38,13 +42,21 @@ public class casco_de_tortuga extends ItemArmor {
 	            	 
 	            	 if(world.isRaining() && !world.isThundering()) {
 		            	 player.addPotionEffect(new PotionEffect(Potion.getPotionById(5),50, 1));
-
+                         soleado = false;
+                         truenos = false;
+                         lloviendo = true;
 	            	 } if(world.isThundering()) {
 	            		 
 	            		player.addPotionEffect(new PotionEffect(Potion.getPotionById(1), 50, 2));
-	            		 
+	            		soleado = false;
+                        truenos = true;
+                        lloviendo = false;
 	            	 }
-	            	 
+	            	 if(!world.isRaining() && !world.isThundering()) {
+	            		 soleado = true;
+                         truenos = false;
+                         lloviendo = false;
+	            	 }
 	              }
 	        }
 	   
@@ -57,7 +69,14 @@ public class casco_de_tortuga extends ItemArmor {
 	
 	 @Override
 		public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-			 tooltip.add(TextFormatting.GREEN + "Descubre los efectos especiales");
+			 tooltip.add(TextFormatting.GREEN + "Bienvenidos al Tiempo");
+			 if(soleado) {
+				 tooltip.add(TextFormatting.LIGHT_PURPLE + "El tiempo para hoy es:" + TextFormatting.GOLD + " Soleado");
+			 }else if(lloviendo) {
+				 tooltip.add(TextFormatting.LIGHT_PURPLE + "El tiempo para hoy es:" + TextFormatting.BLUE + " Lloviendo");
+			 }else if(truenos) {
+				 tooltip.add(TextFormatting.LIGHT_PURPLE + "El tiempo para hoy es:" + TextFormatting.AQUA + " tormenta");
+			 }
 		}
 	 
 }
